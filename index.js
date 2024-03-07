92e737a811a90bc1f62c6449a26b6bbae92658ad
Roy Yang <92054610+Tota1N00b@users.noreply.github.com>
2024-03-06 21:56:39 -0500

    add slidyboy and embed the youtube video

---
+++ index.js
@@ -0,0 +1,8 @@
+function openInsta() {
+    window.open("https://www.instagram.com/teamecho.studio", "_blank");
+}
+
+function toggleSlidyboy() {
+    var contentElement = document.querySelector(".slidyboy");
+    contentElement.classList.toggle("toggled");
+}
