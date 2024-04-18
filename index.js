cf4d437edeecf463075403cbee46a32419e975de
Roy Yang <92054610+Tota1N00b@users.noreply.github.com>
2024-04-18 00:27:45 -0400

    Update 'AboutUs' page

--- index.js
+++ index.js
@@ -2,7 +2,28 @@ function openInsta() {
     window.open("https://www.instagram.com/teamecho.studio", "_blank");
 }
 
-function toggleSlidyboy() {
-    var contentElement = document.querySelector(".slidyboy");
-    contentElement.classList.toggle("toggled");
+function togglePowerParadox() {
+    var powerParadoxElement = document.getElementById("powerParadox");
+    var aboutUsElement = document.getElementById("aboutUs");
+
+    // Check if 'aboutUs' is visible, hide it if yes
+    if (aboutUsElement.classList.contains("toggled")) {
+        aboutUsElement.classList.remove("toggled");
+    }
+
+    // Toggle 'powerParadox' visibility regardless of the current state
+    powerParadoxElement.classList.toggle("toggled");
+}
+
+function toggleAboutUs() {
+    var aboutUsElement = document.getElementById("aboutUs");
+    var powerParadoxElement = document.getElementById("powerParadox");
+
+    // Check if 'powerParadox' is visible, hide it if yes
+    if (powerParadoxElement.classList.contains("toggled")) {
+        powerParadoxElement.classList.remove("toggled");
+    }
+
+    // Toggle 'aboutUs' visibility regardless of the current state
+    aboutUsElement.classList.toggle("toggled");
 }
